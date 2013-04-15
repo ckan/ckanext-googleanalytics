6d7aa555c3b2d554fe74cc0df4186c88965a21de
Sean Hammond <seanhammond@lavabit.com>
2013-04-15 19:55:31 +0200

    Fix resource download event tracking js

--- ckanext/googleanalytics/fanstatic_library/googleanalytics_event_tracking.js
+++ ckanext/googleanalytics/fanstatic_library/googleanalytics_event_tracking.js
@@ -6,10 +6,9 @@ this.ckan.module('google-analytics', function(jQuery, _) {
     },
     initialize: function() {
       jQuery('a.resource-url-analytics').on('click', function() {
-          var resource_prefix = this.options.googleanalytics_resource_prefix;
-          var resource_url = resource_prefix + encodeURIComponent(jQuery(this).prop('href'));
+          var resource_url = encodeURIComponent(jQuery(this).prop('href'));
           if (resource_url) {
-            _gaq.push(['_trackPageview', resource_url]);
+            _gaq.push(['_trackEvent', 'Resource', 'Download', resource_url]);
           }
       });
     }

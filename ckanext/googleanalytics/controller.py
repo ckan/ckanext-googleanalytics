13cc8c28d556a8eaf0939b1ea8f69f9e94402b7d
nigelb <nigelbabu@gmail.com>
2015-02-17 11:05:19 +0530

    Re-land the new tracking from @maxious
    This reverts commit f4fce4760194be9434a7fd3d547eb2e2aceab82f.

--- ckanext/googleanalytics/controller.py
+++ ckanext/googleanalytics/controller.py
@@ -1,8 +1,24 @@
 import logging
-from ckan.lib.base import BaseController, c, render
+from ckan.lib.base import BaseController, c, render, request
 import dbutil
 
+import urllib
+import urllib2
+
+import logging
+import ckan.logic as logic
+import hashlib
+import Queue
+import threading
+from pylons import config
+
+from webob.multidict import UnicodeMultiDict
+from paste.util.multidict import MultiDict
+
+from ckan.controllers.api import ApiController
+
 log = logging.getLogger('ckanext.googleanalytics')
+
 
 class GAController(BaseController):
     def view(self):
@@ -10,3 +26,126 @@ class GAController(BaseController):
         c.top_packages = dbutil.get_top_packages(limit=10)
         c.top_resources = dbutil.get_top_resources(limit=10)
         return render('summary.html')
+
+
+class AnalyticsPostThread(threading.Thread):
+    """Threaded Url POST"""
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            # grabs host from queue
+            data_dict = self.queue.get()
+
+            data = urllib.urlencode(data_dict)
+            log.debug("Sending API event to Google Analytics: " + data)
+            # send analytics
+            urllib2.urlopen(
+                "http://www.google-analytics.com/collect",
+                data,
+                # timeout in seconds
+                # https://docs.python.org/2/library/urllib2.html#urllib2.urlopen
+                10)
+
+            # signals to queue job is done
+            self.queue.task_done()
+
+
+class GAApiController(ApiController):
+    # intercept API calls to record via google analytics
+    analytics_queue = Queue.Queue()
+
+    def __init__(self):
+        # spawn a pool of 5 threads, and pass them queue instance
+        for i in range(5):
+            t = AnalyticsPostThread(self.analytics_queue)
+            t.setDaemon(True)
+            t.start()
+
+    def _post_analytics(
+            self, user, request_obj_type, request_function, request_id):
+        if config.get('googleanalytics.id'):
+            data_dict = {
+                "v": 1,
+                "tid": config.get('googleanalytics.id'),
+                "cid": hashlib.md5(user).hexdigest(),
+                # customer id should be obfuscated
+                "t": "event",
+                "dh": c.environ['HTTP_HOST'],
+                "dp": c.environ['PATH_INFO'],
+                "dr": c.environ.get('HTTP_REFERER', ''),
+                "ec": "CKAN API Request",
+                "ea": request_obj_type+request_function,
+                "el": request_id,
+            }
+            self.analytics_queue.put(data_dict)
+
+    def action(self, logic_function, ver=None):
+        try:
+            function = logic.get_action(logic_function)
+            side_effect_free = getattr(function, 'side_effect_free', False)
+            request_data = self._get_request_data(
+                try_url_params=side_effect_free)
+            if isinstance(request_data, dict):
+                id = request_data.get('id', '')
+                if 'q' in request_data:
+                    id = request_data['q']
+                if 'query' in request_data:
+                    id = request_data['query']
+                self._post_analytics(c.user, logic_function, '', id)
+        except Exception, e:
+            log.debug(e)
+            pass
+
+        return ApiController.action(self, logic_function, ver)
+
+    def list(self, ver=None, register=None,
+             subregister=None, id=None):
+        self._post_analytics(c.user,
+                             register +
+                             ("_"+str(subregister) if subregister else ""),
+                             "list",
+                             id)
+        return ApiController.list(self, ver, register, subregister, id)
+
+    def show(self, ver=None, register=None,
+             subregister=None, id=None, id2=None):
+        self._post_analytics(c.user,
+                             register +
+                             ("_"+str(subregister) if subregister else ""),
+                             "show",
+                             id)
+        return ApiController.show(self, ver, register, subregister, id, id2)
+
+    def update(self, ver=None, register=None,
+               subregister=None, id=None, id2=None):
+        self._post_analytics(c.user,
+                             register +
+                             ("_"+str(subregister) if subregister else ""),
+                             "update",
+                             id)
+        return ApiController.update(self, ver, register, subregister, id, id2)
+
+    def delete(self, ver=None, register=None,
+               subregister=None, id=None, id2=None):
+        self._post_analytics(c.user,
+                             register +
+                             ("_"+str(subregister) if subregister else ""),
+                             "delete",
+                             id)
+        return ApiController.delete(self, ver, register, subregister, id, id2)
+
+    def search(self, ver=None, register=None):
+        id = None
+        try:
+            params = MultiDict(self._get_search_params(request.params))
+            if 'q' in params.keys():
+                id = params['q']
+            if 'query' in params.keys():
+                id = params['query']
+        except ValueError, e:
+            log.debug(str(e))
+            pass
+        self._post_analytics(c.user, register, "search", id)
